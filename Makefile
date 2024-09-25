677e432f4777a2f18ca384caa25caea45b09a712
Michał Górny <mgorny@gentoo.org>
2024-09-25 20:27:47 +0200

    Use dlsym() instead

--- Makefile
+++ Makefile
@@ -1,4 +1,12 @@
-main: main.c libtime32.so libtime64.so
+all: both abi32 abi64
+
+both: main.c libtime32.so libtime64.so
+	$(CC) $(CFLAGS) -o $@ -Wl,-rpath,. $^
+
+abi32: main.c libtime32.so
+	$(CC) $(CFLAGS) -o $@ -Wl,-rpath,. $^
+
+abi64: main.c libtime64.so
 	$(CC) $(CFLAGS) -o $@ -Wl,-rpath,. $^
 
 libtime32.so: lib.c
--- lib.c
+++ lib.c
@@ -1,15 +1,24 @@
+#include <dlfcn.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #if _TIME_BITS == 64
-int __attribute__((weak)) __gentoo_time32 = 0;
 int __gentoo_time64 = 1;
 #else
 int __gentoo_time32 = 1;
-int __attribute__((weak)) __gentoo_time64 = 0;
 #endif
 
 __attribute__((constructor))
 static void __gentoo_check_time_bits()
 {
-	printf("time32 = %d, time64 = %d\n", __gentoo_time32, __gentoo_time64);
+	void *time32_sym = dlsym(RTLD_DEFAULT, "__gentoo_time32");
+	void *time64_sym = dlsym(RTLD_DEFAULT, "__gentoo_time64");
+
+	if (time32_sym != NULL && time64_sym != NULL) {
+		fprintf(stderr,
+				"Both time32 and time64 libraries found in link chain. "
+				"This likely indicates ABI incompatibility. Please make "
+				"sure to rebuild all the executables against the same ABI.\n");
+		abort();
+	}
 }
